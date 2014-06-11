cd10a253c6b39aef75819c4d9fdff1692b5e7596
klehner <klaus.lehner@catalysts.cc>
2014-06-11 15:55:08 +0200

    using application.properties

--- autoconfigure/src/test/java/net/klu2/springboot/autoconfigure/TestConfiguration.java
+++ autoconfigure/src/test/java/net/klu2/springboot/autoconfigure/TestConfiguration.java
@@ -1,9 +1,7 @@
 package net.klu2.springboot.autoconfigure;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.MessageSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -11,6 +9,5 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @EnableAutoConfiguration
-@PropertySource(value = "classpath:test.properties")
 public class TestConfiguration {
 }
